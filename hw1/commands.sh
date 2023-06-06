de40384cf45d29bc1d43d77f5cd5dfed85ab0c2a
bondarev-a <bondarev.a.a@gmail.com>
2023-06-06 23:41:04 +0300

    added commands

--- hw1/commands.sh
+++ hw1/commands.sh
@@ -3,3 +3,7 @@ docker build -t hw1:v1 .
 docker build -f dockerfile -t hw1:v1 .
 docker build -f dockerfile_build -t hw1:v2 .
 docker build -f dockerfile_3 -t hw1:v3 .
+
+docker run hw1:v4 -d -p 8000:8000
+
+docker build -f dockerfile_1 -t hw1:v4 .
