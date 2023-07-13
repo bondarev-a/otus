9f6e8ac42a612e6e658e7297ef961ada9b09bb9e
bondarev-a <bondarev.a.a@gmail.com>
2023-07-13 14:40:45 +0300

    test

--- hw1/commands.sh
+++ hw1/commands.sh
@@ -7,3 +7,9 @@ docker build -f dockerfile_3 -t hw1:v3 .
 docker run hw1:v4 -d -p 8000:8000
 
 docker build -f dockerfile_1 -t hw1:v4 .
+
+docker tag local-image:tagname new-repo:tagname
+
+docker push alekseybondarev/otus:latest
+
+docker tag hw1:v4 alekseybondarev/otus
